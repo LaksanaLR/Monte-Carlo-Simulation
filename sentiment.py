a227f8035e262bfe63a57cafed2508755379218b
LaksanaLR <53202590+LaksanaLR@users.noreply.github.com>
2021-02-25 11:37:28 +0700

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -3,11 +3,11 @@ import textblob
 from textblob import TextBlob
 
 
-consumer_key = 'tIlu5VWiX05objcMa6LmHRFt0'
-consumer_secret = '6gkyT0LTVpAlNrGeBsDFZ4MBupcrMeO7vTkF9PPFqXOZZRVqTd'
+consumer_key = ''
+consumer_secret = ''
 
-access_token = '868415366027853824-0OwHrmttXkYgXvfzkiP84rJlDTdKLQ4'
-access_secret = 'iH4DS0yVayNQvXJ2QgsSVyErxLRj0u3wWaYXlXOmOVZfl'
+access_token = ''
+access_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
